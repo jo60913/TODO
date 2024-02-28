783e093063ef54f5dab0c1cc41234ff5a50027c5
huangweizhe <naruto60913@gmail.com>
2024-02-28 11:31:46 +0800

    新增待辦事項的設定

--- lib/application/page/create_todo_entry/bloc/create_to_do_entry_page_cubit.dart
+++ lib/application/page/create_todo_entry/bloc/create_to_do_entry_page_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/application/core/form_value.dart';
+import 'package:todo/core/use_case.dart';
+import 'package:todo/domain/entity/todo_entry.dart';
 
 import '../../../../domain/entity/unique_id.dart';
 import '../../../../domain/usecase/create_todo_entry.dart';
@@ -11,7 +13,8 @@ class CreateToDoEntryPageCubit extends Cubit<CreateTodoEntryPageState> {
   final CollectionId collectionId;
   final CreateToDoEntry addToDoEntry;
 
-  CreateToDoEntryPageCubit({required this.collectionId,required this.addToDoEntry})
+  CreateToDoEntryPageCubit(
+      {required this.collectionId, required this.addToDoEntry})
       : super(const CreateTodoEntryPageState());
 
   void descriptionChanged({String? description}) {
@@ -21,8 +24,17 @@ class CreateToDoEntryPageCubit extends Cubit<CreateTodoEntryPageState> {
     } else {
       currentStatus = ValidationStatus.success;
     }
-    emit(state.copyWith(description: FormValue(value: description!, validationStatus: currentStatus)));
+    emit(state.copyWith(
+        description:
+            FormValue(value: description!, validationStatus: currentStatus)));
   }
 
-  void submit() {}
+  void submit() async{
+    await addToDoEntry.call(
+      ToDoEntryParams(
+        entry: ToDoEntry.empty().copyWith(
+            description: state.description?.value),
+      ),
+    );
+  }
 }
