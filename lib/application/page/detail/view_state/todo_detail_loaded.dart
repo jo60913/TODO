3d75fc6b2560edfb307b14f5468aec5b6c4f6f82
huangweizhe <naruto60913@gmail.com>
2024-04-28 01:11:27 +0800

    新增android在detail右鍵刪除按鈕

--- lib/application/page/detail/view_state/todo_detail_loaded.dart
+++ lib/application/page/detail/view_state/todo_detail_loaded.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -41,7 +43,12 @@ class _ToDoDetailLoadedState extends State<ToDoDetailLoaded> {
                         entryId: widget.entryIds[index],
                         collectionId: widget.collectionId,
                       ),
-                      onLongPress: (){},
+                      onLongPress: (){
+                        if(Platform.isAndroid){
+                          debugPrint("android按下");
+                          showPopupMenu(context,widget.entryIds[index],widget.collectionId,Offset(50 , 100));
+                        }
+                      },
                       onSecondaryTapDown: (detail){
                         if(kIsWeb){
                           debugPrint("右鍵 entry id ${widget.entryIds[index]}");
